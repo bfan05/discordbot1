fce4319722f48b5870e297146c1798e6a10d09a1
bfan05 <76703988+bfan05@users.noreply.github.com>
2021-06-20 10:16:23 -0700

    Update mute.js

--- commands/mute.js
+++ commands/mute.js
@@ -5,10 +5,10 @@ module.exports = {
         const target = message.mentions.users.first();
         if (target) {
             let mainRole = message.guild.roles.cache.find(role => role.name === 'Member');
-            let muteRole = message.guild.roles.cache.find(role => role.name === 'Muted');
+            let muteRole = message.guild.roles.cache.find(role => role.name === 'admin');
 
             let memberTarget = message.guild.members.cache.get(target.id);
-            
+
             memberTarget.roles.remove(mainRole.id);
             memberTarget.roles.add(muteRole.id);
             message.channel.send(`<@${memberTarget.user.id}> has been muted`);
