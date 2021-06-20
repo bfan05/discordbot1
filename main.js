6091967607362f01094ac2668065ec6dc9f65da4
bfan05 <76703988+bfan05@users.noreply.github.com>
2021-06-20 09:40:17 -0700

    .env

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
-
-const client = new Discord.Client();
+require('dotenv').config();
+const client = new Discord.Client({partials: ["MESSAGE", "CHANNEL", "REACTION"]});
 
 const prefix = '-';
 
@@ -43,4 +43,4 @@ client.on('message', message => {
     }
 });
 
-client.login(process.env.token);
+client.login(process.env.DISCORD_TOKEN);
