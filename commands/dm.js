3efea91cefd990687ca12cd2f4cfb99b29951bbc
bfan05 <76703988+bfan05@users.noreply.github.com>
2021-06-20 14:24:50 -0700

    Update dm.js

--- commands/dm.js
+++ commands/dm.js
@@ -4,6 +4,10 @@ module.exports = {
     execute(message, args) {
         const member = message.mentions.users.first();
         const memberTarget = message.guild.members.cache.get(member.id);
-        memberTarget.send("hi");
+        if (args[2]) {
+            memberTarget.send(args[2]);
+        } else {
+            member.channel.send("please specify what you want to dm!");
+        }
     }
 }
