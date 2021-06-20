7c7f1261a61e4b4c7fa008bed8e8b7484d5164d8
bfan05 <76703988+bfan05@users.noreply.github.com>
2021-06-20 10:17:49 -0700

    Update mute.js

--- commands/mute.js
+++ commands/mute.js
@@ -5,12 +5,12 @@ module.exports = {
         const target = message.mentions.users.first();
         if (target) {
             let mainRole = message.guild.roles.cache.find(role => role.name === 'Member');
-            let muteRole = message.guild.roles.cache.find(role => role.name === 'admin');
+            let muteRole = message.guild.roles.cache.find(role => role.name === 'Muted');
 
             let memberTarget = message.guild.members.cache.get(target.id);
-
-            memberTarget.roles.remove(mainRole.id);
+            
             memberTarget.roles.add(muteRole.id);
+            memberTarget.roles.remove(mainRole.id);
             message.channel.send(`<@${memberTarget.user.id}> has been muted`);
         } else {
             message.channel.send("could not find that member!");
