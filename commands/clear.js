9eae69ba44eb736091d9ca16e7a5f0a0b193a480
bfan05 <76703988+bfan05@users.noreply.github.com>
2021-06-19 22:58:24 -0700

    fixed clear

--- commands/clear.js
+++ commands/clear.js
@@ -6,7 +6,7 @@ module.exports = {
         if (isNaN(args[0])) return message.reply("please enter a real number!");
         if (args[0] > 100) return message.reply("you cannot delete more than 100 messages!");
         if (args[0] < 1) return message.reply("you must delete at least one message!");
-        await message.channel.messages.fetch({limit: args[0] + 1}).then(messages => {
+        await message.channel.messages.fetch({limit: args[0]}).then(messages => {
             message.channel.bulkDelete(messages);
         });
         message.channel.send('successfully cleared ' + args[0] + ' messages!');
