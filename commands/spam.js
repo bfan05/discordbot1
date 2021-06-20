af4d2afa02e0ddfab4edb474a0e325951fe97f16
bfan05 <76703988+bfan05@users.noreply.github.com>
2021-06-19 23:19:40 -0700

    created spam

---
+++ commands/spam.js
@@ -0,0 +1,12 @@
+module.exports = {
+    name: 'spam',
+    description: 'the bot spams',
+    async execute(message, args, Discord) {
+        if (!args[0]) return message.reply("please specify the amount of messages you want to clear!");
+        if (isNaN(args[0])) return message.reply("please enter a real number!");
+        if (args[0] > 100) return message.reply("you cannot spam more than 100 messages!");
+        if (args[0] < 1) return message.reply("you must spam at least one message!");
+        for (let i = 0; i < args[0]; ++i) message.channel.send("spam");
+        message.channel.send("spam complete!");
+    }
+}
