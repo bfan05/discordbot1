745ff7990020210b46efb9b0d0ec9badac58738b
bfan05 <76703988+bfan05@users.noreply.github.com>
2021-06-20 16:29:46 -0700

    Update roll.js

--- commands/roll.js
+++ commands/roll.js
@@ -10,7 +10,7 @@ module.exports = {
             if (isNaN(args[0]) || isNaN(args[1])) return message.channel.send('must enter real numbers!');
             if (args[0] > args[1]) return message.channel.send('must be a valid range!');
             let num = randomNumber(parseInt(args[0]), parseInt(args[1]));
-            message.channel.send(num);
+            message.channel.send(message.author.username + ' you rolled a ' + num + '!');
         }
         else {
             message.channel.send('must enter arguments!');
