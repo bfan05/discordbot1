32ca367e6de86f4185c736610f754c6da49892c0
bfan05 <76703988+bfan05@users.noreply.github.com>
2021-06-20 14:48:03 -0700

    Update dm.js

--- commands/dm.js
+++ commands/dm.js
@@ -5,9 +5,13 @@ module.exports = {
         const member = message.mentions.users.first();
         const memberTarget = message.guild.members.cache.get(member.id);
         if (!member) message.channel.send("i could not find that member!");
-        if (args[1]) {
-            let msg = args[1];
-            let i = 1;
+        if (!args[1]) return message.reply("please specify the amount of messages you want to clear!");
+        if (isNaN(args[1])) return message.reply("please enter a real number!");
+        if (args[1] > 100) return message.reply("you cannot dm more than 100 messages!");
+        if (args[1] < 1) return message.reply("you must dm at least one message!");
+        if (args[2]) {
+            let msg = args[2];
+            let i = 2;
             while (args[i + 1]) {
                 msg += " " + args[i + 1];
                 ++i;
