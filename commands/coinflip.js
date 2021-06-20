1454a4844acbced3c55eec5a62e9c5e8b6bd9482
bfan05 <76703988+bfan05@users.noreply.github.com>
2021-06-19 22:38:07 -0700

    coin lands on side

--- commands/coinflip.js
+++ commands/coinflip.js
@@ -6,7 +6,9 @@ module.exports = {
     name: 'coinflip',
     description: 'flips a coin',
     execute(message, args, Discord) {
-        if (randomNumber(1, 2) % 2 == 0) message.channel.send('heads!');
+        let flip = randomNumber(1, 100);
+        if (flip >= 1 && flip < 21) message.channel.send('the coin lands on its side!');
+        else if (flip % 2 == 0) message.channel.send('heads!');
         else message.channel.send('tails!');
     }
 }
