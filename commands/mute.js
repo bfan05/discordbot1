59e07c1436fda3905c25c5e5d0d229a4084395dd
bfan05 <76703988+bfan05@users.noreply.github.com>
2021-06-20 15:56:49 -0700

    Update mute.js

--- commands/mute.js
+++ commands/mute.js
@@ -1,4 +1,4 @@
-const ms = require('ms');
+/*const ms = require('ms');
 module.exports = {
     name: 'mute',
     description: 'mutes a member',
@@ -25,6 +25,26 @@ module.exports = {
                 memberTarget.roles.remove(muteRole);
                 memberTarget.roles.add(mainRole);
             }, ms(args[1]));
+        } else {
+            message.channel.send("could not find that member!");
+        }
+    }
+}*/
+
+module.exports = {
+    name: 'unmute',
+    description: 'unmutes a member',
+    execute(message, args) {
+        const target = message.mentions.users.first();
+        if (target) {
+            let mainRole = message.guild.roles.cache.find(role => role.name === 'Member');
+            let muteRole = message.guild.roles.cache.find(role => role.name === 'muted');
+
+            let memberTarget = message.guild.members.cache.get(target.id);
+            
+            memberTarget.roles.add(mainRole);
+            memberTarget.roles.remove(muteRole);
+            message.channel.send(`<@${memberTarget.user.id}> has been unmuted`);
         } else {
             message.channel.send("could not find that member!");
         }
