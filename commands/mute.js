9014f46907354064f107f9e20b7380140d3a40a0
bfan05 <76703988+bfan05@users.noreply.github.com>
2021-06-20 10:30:50 -0700

    edited mute

--- commands/mute.js
+++ commands/mute.js
@@ -1,3 +1,4 @@
+const ms = require('ms');
 module.exports = {
     name: 'mute',
     description: 'mutes a member',
@@ -8,10 +9,22 @@ module.exports = {
             let muteRole = message.guild.roles.cache.find(role => role.name === 'muted');
 
             let memberTarget = message.guild.members.cache.get(target.id);
-            
+
+            if (!args[1]) {
+                memberTarget.roles.add(muteRole);
+                memberTarget.roles.remove(mainRole);
+                message.channel.send(`<@${memberTarget.user.id}> has been muted`);
+                return;
+            }
+
             memberTarget.roles.add(muteRole);
             memberTarget.roles.remove(mainRole);
-            message.channel.send(`<@${memberTarget.user.id}> has been muted`);
+            message.channel.send(`<@${memberTarget.user.id}> has been muted for ${ms(ms(args[1]))}`);
+
+            setTimeout(function () {
+                memberTarget.roles.remove(muteRole);
+                memberTarget.roles.add(mainRole);
+            }, ms(args[1]));
         } else {
             message.channel.send("could not find that member!");
         }
