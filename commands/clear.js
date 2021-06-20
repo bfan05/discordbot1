00690606e02885ece52c97706b3bc20ed143ba02
bfan05 <76703988+bfan05@users.noreply.github.com>
2021-06-19 22:53:23 -0700

    changed int to NaN

--- commands/clear.js
+++ commands/clear.js
@@ -3,7 +3,7 @@ module.exports = {
     description: 'clear messages',
     async execute(message, args, Discord) {
         if (!args[0]) return message.reply("please specify the amount of messages you want to clear!");
-        if (!isInteger(args[0])) return message.reply("please enter a real number!");
+        if (isNaN(args[0])) return message.reply("please enter a real number!");
         if (args[0] > 100) return message.reply("you cannot delete more than 100 messages!");
         if (args[0] < 1) return message.reply("you must delete at least one message!");
         await message.channel.messages.fetch({limit: args[0]}).then(messages => {
