11a8ce6062285626f86815c7abd6231644b80783
bfan05 <76703988+bfan05@users.noreply.github.com>
2021-06-21 15:16:04 -0700

    Update guildMemberAdd.js

--- events/guild/guildMemberAdd.js
+++ events/guild/guildMemberAdd.js
@@ -1,4 +1,4 @@
-module.exports = (Discord, client, message) => {
+module.exports = (Discord, client, guildMember) => {
     let welcomeRole = guildMember.guild.roles.cache.find(role => role.name === 'Member');
     guildMember.roles.add(welcomeRole);
     guildMember.guild.channels.cache.get('856397188913299506').send(`welcome <@${guildMember.user.id}> to bruhphastars!`);
