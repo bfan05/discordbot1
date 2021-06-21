0de5294cba6c6e79f96c89b70419b7450193024a
bfan05 <76703988+bfan05@users.noreply.github.com>
2021-06-21 15:19:16 -0700

    updated roll and coinflip

--- commands/coinflip.js
+++ commands/coinflip.js
@@ -7,8 +7,8 @@ module.exports = {
     description: 'flips a coin',
     execute(client, message, args, Discord) {
         let flip = randomNumber(1, 100);
-        if (flip >= 1 && flip < 21) message.channel.send('the coin lands on its side!');
-        else if (flip % 2 == 0) message.channel.send('heads!');
-        else message.channel.send('tails!');
+        if (flip == 1) message.channel.send('🪙... the coin lands on its side!');
+        else if (flip % 2 == 0) message.channel.send('🪙... heads!');
+        else message.channel.send('🪙... tails!');
     }
 }
