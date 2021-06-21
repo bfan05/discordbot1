3ca0541f4058c8f5262dd181262bac6cb550e041
bfan05 <76703988+bfan05@users.noreply.github.com>
2021-06-21 16:55:47 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 require('dotenv').config();
 const client = new Discord.Client({partials: ["MESSAGE", "CHANNEL", "REACTION"]});
-
+const mongoose = require('mongoose');
 
 const memberCounter = require('./counters/member-counter');
 
@@ -10,6 +10,17 @@ client.events = new Discord.Collection();
 
 ['command_handler', 'event_handler'].forEach(handler => {
     require(`./handlers/${handler}`)(client, Discord);
+});
+
+mongoose.connect(process.env.mongodb_srv, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+}).then(() => {
+    console.log('Connected to the database!');
+})
+.catch((err) => {
+    console.log(err);
 });
 
 client.login(process.env.token);
