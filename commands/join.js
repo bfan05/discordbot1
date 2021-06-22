b74bb6c18a3d968464ddc860d741f4916c3315e2
bfan05 <76703988+bfan05@users.noreply.github.com>
2021-06-21 20:31:05 -0700

    Update join.js

--- commands/join.js
+++ commands/join.js
@@ -6,6 +6,6 @@ module.exports = {
     permissions: ['MANAGE_EMOJIS'],
     description: 'join the bot!',
     execute(client, message, args, Discord, profileModel) {
-        message.channel.send(`<@${message.author.id}>, thanks for joining **TMC Bot**! Enjoy a free **10** TMC Cash!`);
+        message.channel.send(`<@${message.author.id}>, thanks for joining **TMC Bot**!`);
     }
 }
