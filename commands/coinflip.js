f73f3e18e8424b268157172033383a1ef1525a77
bfan05 <76703988+bfan05@users.noreply.github.com>
2021-06-21 19:42:21 -0700

    Update coinflip.js

--- commands/coinflip.js
+++ commands/coinflip.js
@@ -21,29 +21,30 @@ module.exports = {
         && args[1] != 'tails' && args[1] != 't')) return message.reply('You must choose heads or tails!');
         let update = 0;
         let totalUpdate = 0;
+        let bet = Math.floor(args[0]);
 
         let flip = randomNumber(1, 101);
         if (flip == 1) {
-            message.channel.send(`${message.author.username} bet **${args[0]}**... 🪙 **|** the coin lands on its **side**! You won **${args[0]}** TMC Cash!`);
+            message.channel.send(`${message.author.username} bet **${bet}**... 🪙 **|** the coin lands on its **side**! You won **${bet}** TMC Cash!`);
         }
         else if(flip % 2 == 0) {
             if (args[1] == 'heads' || args[1] == 'h') {
-                update = args[0];
-                message.channel.send(`${message.author.username} bet **${args[0]}** and chose heads... 🪙 **|** the coin lands on **heads**! You won **${2 * args[0]}** TMC Cash!`);
+                update = bet;
+                message.channel.send(`${message.author.username} bet **${bet}** and chose heads... 🪙 **|** the coin lands on **heads**! You won **${2 * bet}** TMC Cash!`);
             }
             else {
-                update = -args[0];
-                message.channel.send(`${message.author.username} bet **${args[0]}** and chose heads... 🪙 **|** the coin lands on **heads**! You lost it all...`);
+                update = -bet;
+                message.channel.send(`${message.author.username} bet **${bet}** and chose heads... 🪙 **|** the coin lands on **heads**! You lost it all...`);
             }
         }
         else {
             if (args[1] == 'tails' || args[1] == 't') {
-                update = args[0];
-                message.channel.send(`${message.author.username} bet **${args[0]}** and chose tails... 🪙 **|** the coin lands on **tails**! You won **${2 * args[0]}** TMC Cash!`);
+                update = bet;
+                message.channel.send(`${message.author.username} bet **${bet}** and chose tails... 🪙 **|** the coin lands on **tails**! You won **${2 * bet}** TMC Cash!`);
             }
             else {
-                update = -args[0];
-                message.channel.send(`${message.author.username} bet **${args[0]}** and chose tails... 🪙 **|** the coin lands on **tails**! You lost it all...`);
+                update = -bet;
+                message.channel.send(`${message.author.username} bet **${bet}** and chose tails... 🪙 **|** the coin lands on **tails**! You lost it all...`);
             }
         }
         if (update > 0) totalUpdate = update;
