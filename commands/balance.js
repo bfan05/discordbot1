15e88070b42aa23e1e039dbf682f0fb8188d6dc7
bfan05 <76703988+bfan05@users.noreply.github.com>
2021-06-21 18:17:58 -0700

    Update balance.js

--- commands/balance.js
+++ commands/balance.js
@@ -6,12 +6,12 @@ module.exports = {
         .setColor('#9CCFE7')
         .setTitle(`**${message.author.username}**'s TMC Cash`)
         .setThumbnail(message.author.avatarURL)
-        .setURL('https://media.discordapp.net/attachments/831654793754968074/849919810611314748/eed9f8da-327e-4646-b24b-e916c6cca394.gif')
         .addFields(
-            {name: 'Wallet:', value: `${profileData.coins}𝕋`},
-            {name: 'Bank:', value: `${profileData.bank}𝕋`},
+            {name: 'Wallet:', value: `${profileData.coins} 𝕋`},
+            {name: 'Bank:', value: `${profileData.bank} 𝕋`},
         )
-
+        .setImage('https://media.discordapp.net/attachments/831654793754968074/849919810611314748/eed9f8da-327e-4646-b24b-e916c6cca394.gif')
+        
         message.channel.send(newEmbed);
     }
 }
