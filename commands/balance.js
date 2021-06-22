3d805891449c4f290a9ce08ee7657a162bac47eb
bfan05 <76703988+bfan05@users.noreply.github.com>
2021-06-21 18:30:43 -0700

    Update balance.js

--- commands/balance.js
+++ commands/balance.js
@@ -4,12 +4,12 @@ module.exports = {
     execute(client, message, args, Discord, profileData) {
         const newEmbed = new Discord.MessageEmbed()
         .setColor('#9CCFE7')
-        .setAuthor(`**${message.author.username}**'s TMC Cash`)
+        .setAuthor(`${message.author.username}'s TMC Cash`, `${message.author.avatarURL}`)
+        .setThumbnail('http://www.simpleimageresizer.com/_uploads/photos/fd03d8aa/tmc_1_20.gif')
         .addFields(
             {name: 'Wallet:', value: `${profileData.coins} 𝕋`},
             {name: 'Bank:', value: `${profileData.bank} 𝕋`},
         )
-        .setImage('http://www.simpleimageresizer.com/_uploads/photos/fd03d8aa/tmc_1_20.gif')
 
         message.channel.send(newEmbed);
     }
