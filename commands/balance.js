a565a00a677dc9ea00903ac9145e94b722768f67
bfan05 <76703988+bfan05@users.noreply.github.com>
2021-06-21 18:14:49 -0700

    Update balance.js

--- commands/balance.js
+++ commands/balance.js
@@ -5,9 +5,11 @@ module.exports = {
         const newEmbed = new Discord.MessageEmbed()
         .setColor('#9CCFE7')
         .setTitle(`**${message.author.username}**'s TMC Cash`)
+        .setThumbnail(message.author.avatar)
+        .setURL('https://media.discordapp.net/attachments/831654793754968074/849919810611314748/eed9f8da-327e-4646-b24b-e916c6cca394.gif')
         .addFields(
-            {name: 'Wallet', value: `𝕋${profileData.coins}`},
-            {name: 'Bank', value: `𝕋${profileData.bank}`},
+            {name: 'Wallet:', value: `${profileData.coins}𝕋`},
+            {name: 'Bank:', value: `${profileData.bank}𝕋`},
         )
 
         message.channel.send(newEmbed);
