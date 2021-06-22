e5bfb409487120b5180b3fee8e16aeb6d446cd0a
bfan05 <76703988+bfan05@users.noreply.github.com>
2021-06-21 21:41:29 -0700

    Create reset.js

---
+++ commands/reset.js
@@ -0,0 +1,28 @@
+const profileModel = require("../models/profileSchema");
+
+module.exports = {
+    name: 'reset',
+    aliases: ['rs'],
+    permissions: ['ADMINISTRATOR'],
+    description: "Reset someone's TMC Cash",
+    async execute(client, message, args, Discord, profileData) {
+        if (!args.length) return message.channel.send('You need to mention a member to reset their TMC Cash!');
+        const data = await profileModel.findOne({ userID: message.author.id });
+
+        const targetData = await profileModel.findOne({ userID: target.id });
+        if (!targetData) return message.channel.send(`This user doesn't exist in the database. Tell him or her to use the -join command!`);
+
+        const response = await profileModel.findOneAndUpdate(
+            {
+                userID: target.id,
+            }, 
+            {
+                $inc: {
+                    coins: -targetData.coins,
+                    total: -targetData.total
+                },
+            }
+        );
+        message.channel.send(`**${target.username}**'s TMC Cash has been reset!`);
+    }
+}
