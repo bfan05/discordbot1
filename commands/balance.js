6893b4870dd31f6a14ad8a44634fc3d508ae8636
bfan05 <76703988+bfan05@users.noreply.github.com>
2021-06-21 17:41:43 -0700

    Update balance.js

--- commands/balance.js
+++ commands/balance.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'balance',
     description: 'check your balance!',
     execute(client, message, args, Discord, profileData) {
-        message.channel.send(`your wallet currently has ${profileData.coins} jj cash! your bank currently has ${profileData.bank} jj cash!`);
+        message.channel.send(`your wallet currently has **${profileData.coins}** jj cash! your bank currently has **${profileData.bank}** jj cash!`);
     }
 }
