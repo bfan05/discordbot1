b85984865d4d02697983686d27069f70e3e427ea
bfan05 <76703988+bfan05@users.noreply.github.com>
2021-06-21 17:37:11 -0700

    Create beg.js

---
+++ commands/beg.js
@@ -0,0 +1,20 @@
+const profileModel = require('../models/profileSchema')
+
+module.exports = {
+    name: 'beg',
+    description: 'beg for coins',
+    async execute(client, message, args, Discord, profileData) {
+        const randomNumber = Math.floor(Math.random * 200) + 1;
+        const response = await profileModel.findOneAndUpdate(
+            {
+                userID: message.author.id,
+            }, 
+            {
+                $inc: {
+                    coins: randomNumber,
+                },
+            }
+        );
+        return message.channel.send(`**${message.author.username}** you begged and received **${randomNumber}** jj cash!`);
+    },
+}
