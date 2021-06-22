bd3aaf8c09760a2c2dd0539bcbbc13058781445d
bfan05 <76703988+bfan05@users.noreply.github.com>
2021-06-22 08:49:13 -0700

    Update message.js

--- events/guild/message.js
+++ events/guild/message.js
@@ -28,24 +28,29 @@ module.exports = async (Discord, client, message) => {
     
     const command = client.commands.get(cmd) || client.commands.find(a => a.aliases && a.aliases.includes(cmd));
 
-    if (!cooldowns.has(command.name)) {
+    if(!cooldowns.has(command.name)){
         cooldowns.set(command.name, new Discord.Collection());
     }
 
-    const currentTime = Date.now();
-    const timeStamps = cooldowns.get(command.name);
-    const cooldownAmount = (command.cooldown) * 1000;
+    const current_time = Date.now();
+    const time_stamps = cooldowns.get(command.name);
+    const cooldown_amount = (command.cooldown) * 1000;
 
-    if (timeStamps.has(message.author.id)) {
-        const expirationTime = timeStamps.get(message.author.id) + cooldownAmount;
-        if (currentTime < expirationTime) {
-            const timeLeft = (expirationTime - currentTime) / 1000;
-            return message.reply(`Please wait ${timeLeft.toFixed(1)} more seconds before using ${command.name}`);
+    //If time_stamps has a key with the author's id then check the expiration time to send a message to a user.
+    if(time_stamps.has(message.author.id)){
+        const expiration_time = time_stamps.get(message.author.id) + cooldown_amount;
+
+        if(current_time < expiration_time){
+            const time_left = (expiration_time - current_time) / 1000;
+
+            return message.reply(`Please wait ${time_left.toFixed(1)} more seconds before using ${command.name}`);
         }
     }
 
-    timeStamps.set(message.author.id, currentTime);
-    setTimeout(() => timeStamps.delete(message.author.id, cooldownAmount));
+    //If the author's id is not in time_stamps then add them with the current time.
+    time_stamps.set(message.author.id, current_time);
+    //Delete the user's id once the cooldown is over.
+    setTimeout(() => time_stamps.delete(message.author.id), cooldown_amount);
 
     const validPermissions = [
         "CREATE_INSTANT_INVITE",
