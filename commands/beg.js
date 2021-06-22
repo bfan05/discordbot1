59c6448b7b7f0bcf161d5eef99223a3476bb4bf0
bfan05 <76703988+bfan05@users.noreply.github.com>
2021-06-21 17:40:12 -0700

    Update beg.js

--- commands/beg.js
+++ commands/beg.js
@@ -4,7 +4,7 @@ module.exports = {
     name: 'beg',
     description: 'beg for coins',
     async execute(client, message, args, Discord, profileData) {
-        const randomNumber = Math.floor(Math.random * 200) + 1;
+        const randomNumber = Math.floor(Math.random() * 200) + 1;
         const response = await profileModel.findOneAndUpdate(
             {
                 userID: message.author.id,
