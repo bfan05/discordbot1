7f25da394efdcf6a1aa95c6ae55e6a2bbae8f6a9
bfan05 <76703988+bfan05@users.noreply.github.com>
2021-06-22 09:22:44 -0700

    Update daily.js

--- commands/daily.js
+++ commands/daily.js
@@ -15,6 +15,7 @@ module.exports = {
             {
                 $inc: {
                     coins: randomNumber,
+                    total: randomNumber,
                 },
             }
         );
