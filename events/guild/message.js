ed765983c3c24c21fa06549d487e77dce6a055dd
bfan05 <76703988+bfan05@users.noreply.github.com>
2021-06-21 17:20:55 -0700

    Update message.js

--- events/guild/message.js
+++ events/guild/message.js
@@ -1,7 +1,24 @@
 require('dotenv').config();
-module.exports = (Discord, client, message) => {
+const profileModel = require('../../models/profileSchema');
+module.exports = async (Discord, client, message) => {
     const prefix = process.env.bot_prefix;
     if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    let profileData;
+    try {
+        profileData = await profileModel.findOne({ userID: message.author.id });
+        if (!profileData) {
+            let profile = await profileModel.create({
+                userID: message.author.id,
+                serverID: message.guild.id,
+                coins: 1000,
+                bank: 0,
+            });
+            profile.save();
+        }
+    } catch (err) {
+        console.log(err)
+    }
 
     const args = message.content.slice(prefix.length).split(/ +/);
     const cmd = args.shift().toLowerCase();
