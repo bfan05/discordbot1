c693e241f388bc35fe39a261bb97478cd0584387
bfan05 <76703988+bfan05@users.noreply.github.com>
2021-06-21 20:57:07 -0700

    Update join.js

--- commands/join.js
+++ commands/join.js
@@ -3,7 +3,7 @@ const profileModel = require('../models/profileSchema')
 module.exports = {
     name: 'join',
     aliases: [],
-    permissions: ['MANAGE_EMOJIS'],
+    permissions: [],
     description: 'join the bot!',
     execute(client, message, args, Discord, profileModel) {
         message.channel.send(`<@${message.author.id}>, thanks for joining **TMC Bot**!`);
