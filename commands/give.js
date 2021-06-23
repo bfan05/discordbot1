34b658ee78a37f6d504b770694a42c7adb3ba9aa
bfan05 <76703988+bfan05@users.noreply.github.com>
2021-06-23 11:50:31 -0700

    Update give.js

--- commands/give.js
+++ commands/give.js
@@ -46,6 +46,6 @@ module.exports = {
             }
         );
         message.channel.send(`**${message.author.username}** sent **${amount.toLocaleString()}** TMC Cash to **${target.username}**!`);
-        client.channels.get('857330940631187526').send(`**${message.author.username}** sent **${amount.toLocaleString()}** TMC Cash to **${target.username}**!`);
+        client.channels.cache.get('857330940631187526').send(`**${message.author.username}** sent **${amount.toLocaleString()}** TMC Cash to **${target.username}**!`);
     }
 }
