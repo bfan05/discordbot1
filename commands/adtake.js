5c2cfffbbaa69c32d86b99f2fd34e9efd5d92324
bfan05 <76703988+bfan05@users.noreply.github.com>
2021-06-23 16:01:51 -0700

    Create adtake.js

---
+++ commands/adtake.js
@@ -0,0 +1,34 @@
+const profileModel = require("../models/profileSchema");
+
+module.exports = {
+    name: 'adtake',
+    aliases: [],
+    permissions: ["ADMINISTRATOR"],
+    description: 'Admins, take some TMC Cash from a member!',
+    async execute(client, message, args, Discord, profileData) {
+        if (!args.length) return message.channel.send('You need to mention a member to take TMC Cash from them!');
+
+        const target = message.mentions.users.first();
+        if (!target) return message.channel.send("That user does not exist!");
+
+        if (!args[1]) return message.channel.send('Please specify how much you want to take!');
+        if (isNaN(args[1])) return message.channel.send("Please enter a real number!");
+        if (args[1] <= 0) return message.channel.send("You must take a positive amount, silly!");
+        if (args[1] > target.coins) return message.channel.send("You can't take more than they have, silly!");
+
+        const targetData = await profileModel.findOne({ userID: target.id });
+        if (!targetData) return message.channel.send(`This user doesn't exist in the database. Tell him or her to use the -join command!`);
+
+        const response = await profileModel.findOneAndUpdate(
+            {
+                userID: target.id,
+            }, 
+            {
+                $inc: {
+                    coins: -amount,
+                },
+            }
+        );
+        message.channel.send(`**${target.username}** lost **${amount.toLocaleString()}** TMC Cash. Boohoo!`);
+    }
+}
