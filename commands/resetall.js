5e101fcaf6308e9a3c23a5e00706d1ee20a847ec
bfan05 <76703988+bfan05@users.noreply.github.com>
2021-06-23 15:05:50 -0700

    Update resetall.js

---
+++ commands/resetall.js
@@ -0,0 +1,16 @@
+const profileModel = require("../models/profileSchema");
+
+module.exports = {
+    name: 'resetall',
+    aliases: ['rsall'],
+    permissions: ['ADMINISTRATOR'],
+    description: "Reset everyone's TMC Cash",
+    async execute(client, message, args, Discord, profileData) {
+        profileModel.find().then((data) => {
+            data.forEach((prof) => {
+                prof.coins = 0;
+                prof.total = 0;
+            });
+        }); 
+    }
+}
