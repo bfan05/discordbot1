00d6f9f3442f316578cd6afb7b30444fc9c72e19
bfan05 <76703988+bfan05@users.noreply.github.com>
2021-06-23 14:21:51 -0700

    Update total.js

--- commands/total.js
+++ commands/total.js
@@ -1,20 +1,38 @@
 const profileModel = require("../models/profileSchema");
 
-module.exports = {
+module.exports.run = async(client, message, args, Discord) => {
+    message.channel.send("hello");
+}
+
+module.exports.help = {
+    name: 'total',
+    aliases: [],
+    permissions: [],
+    description: 'Check the TMC Cash leaderboard!',
+}
+
+
+/*module.exports = {
     name: 'total',
     aliases: [],
     permissions: [],
     description: 'Check the total TMC Cash in circulation.',
     async execute(client, message, args, Discord, profileData) {
+        profileModel.find({
+            lb: 'all'
+        }).sort([
+            ['coins', 'descending']
+        ])
+
         profileModel.find().sort({coins: -1}).fetch(function(err, result) {
             if (err) throw err;
             console.log(result);
         })
-        /* profileModel.find({}).sort({coins: -1});
+        profileModel.find({}).sort({coins: -1});
         const response = await profileModel.find().then((data) => {
             data.forEach((prof) => {
                 message.channel.send(`**${prof.usernm}** currently has **${prof.coins}** TMC Cash!`);
             });
-        }); */
+        }); 
     }
-}
+}*/
