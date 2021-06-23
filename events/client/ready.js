14e97ee11994f09b7321a5f6c4294924bd35fb93
bfan05 <76703988+bfan05@users.noreply.github.com>
2021-06-23 09:24:01 -0700

    Update ready.js

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,4 +1,4 @@
 module.exports = (Discord, client) => {
     console.log('SigmaBot is online!');
-    client.user.setActivity("Type -help for help!");
+    client.user.setActivity(`${client.guilds.cache.size} servers! Type -help for help.`, { type: 'WATCHING' });
 }
