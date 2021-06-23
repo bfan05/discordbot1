e9ca9b130eaa36e6abca19cd65b2a6f482ce9a3a
bfan05 <76703988+bfan05@users.noreply.github.com>
2021-06-23 13:28:35 -0700

    Update profileSchema.js

--- models/profileSchema.js
+++ models/profileSchema.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const profileSchema = new mongoose.Schema({
     userID: {type: String, require: true, unique: true},
     serverID: {type: String, require: true, unique: false},
+    usernm: {type: String, require: true, unique: false},
     coins: {type: Number, default: 0},
     total: {type: Number, default: 0},
 })
