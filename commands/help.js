92a63e0c164118025e99acee7b09f9025d0e28a9
bfan05 <76703988+bfan05@users.noreply.github.com>
2021-06-23 10:22:53 -0700

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -10,13 +10,13 @@ module.exports = {
         .setDescription("Below are commands and how to use them!")
         .setThumbnail('http://www.simpleimageresizer.com/_uploads/photos/fd03d8aa/tmc_2_15.gif')
         .addFields(
-            {name: '-join', value: 'Join the bot! Using this command will get you on your way to becoming rich!'},
-            {name: '-give/send @[member] [amount]', value: 'Give another member some of your TMC Cash!'},
-            {name: '-balance/bal', value: 'Check your TMC Cash balance!'},
-            {name: '-daily', value: 'Claim your daily TMC Cash! Must wait 24 hours before using again.'},
-            {name: '-coinflip/cf [amount] [heads/h or tails/t]', value: 'Are you feeling lucky? Coinflip to win some cash!'},
-            {name: '-adgive @[member] [amount]', value: 'Only for admins. Reward someone with TMC Cash!'},
-            {name: 'reset @[member]', value: 'Only for admins. Someone has been naughty! Reset their cash.'}
+            {name: '-join', value: 'Join the bot! Using this command will get you on your way to becoming rich!', inline: true},
+            {name: '-give/send @[member] [amount]', value: 'Give another member some of your TMC Cash!', inline: true},
+            {name: '-balance/bal', value: 'Check your TMC Cash balance!', inline: true},
+            {name: '-daily', value: 'Claim your daily TMC Cash! Must wait 24 hours before using again.', inline: true},
+            {name: '-coinflip/cf [amount] [heads/h or tails/t]', value: 'Are you feeling lucky? Coinflip to win some cash!', inline: true},
+            {name: '-adgive @[member] [amount]', value: 'Only for admins. Reward someone with TMC Cash!', inline: true},
+            {name: 'reset @[member]', value: 'Only for admins. Someone has been naughty! Reset their cash.', inline: true}
         )
         .setFooter('Created by bfan#0125');
 
