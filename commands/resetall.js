b36abebebe16a4e8b585cf91d56685953e2358e9
bfan05 <76703988+bfan05@users.noreply.github.com>
2021-06-23 15:34:02 -0700

    Update resetall.js

--- commands/resetall.js
+++ commands/resetall.js
@@ -6,22 +6,19 @@ module.exports = {
     permissions: ['ADMINISTRATOR'],
     description: "Reset everyone's TMC Cash",
     async execute(client, message, args, Discord, profileData) {
-        profileModel.find().then((data) => {
-            data.forEach(async (prof) => {
-                let profData = profileModel.findOne({ userID: prof.id });
-                profileModel.findOneAndUpdate(
-                    {
-                        userID: prof.id,
-                    }, 
-                    {
-                        $inc: {
-                            coins: -profData.coins,
-                            total: -profData.total
-                        },
-                    }
-                );
+        let bulkOp = profileModel.initializeOrderedBulkOp();
+        let count = 0;
+        profileModel.find().forEach(function(data) {
+            bulkOp.find({ '_id': doc._id }).updateOne({
+                '$set': { coins: 0, total: 0 }
             });
-        }); 
-        message.channel.send('Reset complete!');
+            count++;
+            if (count % 100 == 0) {
+                bulkOp.execute();
+                bulkOp = collection.initializeOrderedBulkOp();
+            }
+        });
+
+        if (count > 0) bulkOp.execute();
     }
 }
