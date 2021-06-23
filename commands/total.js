acb62e15d987b03d0aa7b26cf315c639fb73c525
bfan05 <76703988+bfan05@users.noreply.github.com>
2021-06-23 13:00:45 -0700

    Update total.js

--- commands/total.js
+++ commands/total.js
@@ -6,6 +6,6 @@ module.exports = {
     permissions: [],
     description: 'Check the total TMC Cash in circulation.',
     async execute(client, message, args, Discord, profileData) {
-        db.getCollection('profilemodels').find({}).forEach(function(data) {message.channel.send(`${data.userID} currently has ${data.coins} TMC Cash.`)})
+        profileModel.find({}).forEach(function(data) {message.channel.send(`${data.userID} currently has ${data.coins} TMC Cash.`)})
     }
 }
