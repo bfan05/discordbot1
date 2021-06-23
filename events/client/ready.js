692cec92fea71b330dd9620435b9ca3f4d5dd2e4
bfan05 <76703988+bfan05@users.noreply.github.com>
2021-06-23 09:14:43 -0700

    Update ready.js

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,3 +1,10 @@
 module.exports = () => {
     console.log('SigmaBot is online!');
+    client.user.setPresence({
+        status: "online",
+        game: {
+            name: "type -help for help!",  //The message shown
+            type: "WATCHING" //PLAYING: WATCHING: LISTENING: STREAMING:
+        }
+    });
 }
