87d711fa9c91a16fee882d13f0511491584f3054
bfan05 <76703988+bfan05@users.noreply.github.com>
2021-06-23 16:21:33 -0700

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -11,9 +11,9 @@ module.exports = {
         .setThumbnail('http://www.simpleimageresizer.com/_uploads/photos/fd03d8aa/tmc_2_15.gif')
         .addFields(
             {name: '-join', value: 'Join the bot! Using this command will get you on your way to becoming rich!'},
-            {name: '-give/send @[member] [amount]', value: 'Give another member some of your TMC Cash!'},
+            {name: '-give/send @[member] [amount]', value: 'Give another member some of your TMC Cash! Let\'s hope they deserved it...'},
             {name: '-balance/bal', value: 'Check your TMC Cash balance!'},
-            {name: '-leaderboard/lb [page number]', value: 'Check the TMC Cash leaderboard!'},
+            {name: '-leaderboard/lb [page number]', value: 'Check the TMC Cash leaderboard! Are you #1?'},
             {name: '-daily', value: 'Claim your daily TMC Cash! Must wait 24 hours before using again.'},
             {name: '-adtake @[member] [amount]', value: 'Only for admins. Take some TMC Cash from a member. No regrets!'},
             {name: '-adgive @[member] [amount]', value: 'Only for admins. Reward someone with TMC Cash!'},
