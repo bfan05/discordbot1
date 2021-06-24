f0f4bb93ec5b2796997b14ce4e735bc120909309
bfan05 <76703988+bfan05@users.noreply.github.com>
2021-06-23 17:39:53 -0700

    Update yawn.js

--- commands/yawn.js
+++ commands/yawn.js
@@ -4,6 +4,15 @@ module.exports = {
     permissions: [],
     description: 'Are you tired?',
     async execute(client, message, args, Discord) {
-        message.channel.send(`<:zzmilkyawn:857418851757850685>`);
+        if (!args[0]) return message.reply("Please specify the number of yawns!");
+        if (isNaN(args[0])) return message.reply("Please enter a real number!");
+        if (args[0] > 100) return message.reply("You cannot yawn more than 100 times!");
+        if (args[0] < 1) return message.reply("You must yawn at least once!");
+
+        let numYawns = Math.floor(args[0]);
+
+        for (let i = 0; i < numYawns; ++i) {
+            message.channel.send(`<:zzmilkyawn:857418851757850685>`);
+        }
     }
 }
