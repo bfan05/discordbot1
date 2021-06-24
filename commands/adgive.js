1516aab353220fb9127734e767139f5b6d3780df
bfan05 <76703988+bfan05@users.noreply.github.com>
2021-06-24 12:20:15 -0700

    changed audit log

--- commands/adgive.js
+++ commands/adgive.js
@@ -32,7 +32,7 @@ module.exports = {
             }
         );
         message.channel.send(`**${target.username}** gained **${amount.toLocaleString()}** TMC Cash!`);
-        client.channels.cache.get('857330940631187526').send(`Admin **${message.author.username} (${message.author.id})** sent **${amount.toLocaleString()}** to **${target.username} (${target.id})**.`);
-        client.channels.cache.get('857698338051588127').send(`Admin **${message.author.username} (${message.author.id})** sent **${amount.toLocaleString()}** to **${target.username} (${target.id})**.`);
+        client.channels.cache.get('857330940631187526').send(`**${message.author.username} (${message.author.id})** adgave **${amount.toLocaleString()}** to **${target.username} (${target.id})**.`);
+        client.channels.cache.get('857698338051588127').send(`**${message.author.username} (${message.author.id})** adgave **${amount.toLocaleString()}** to **${target.username} (${target.id})**.`);
     }
 }
