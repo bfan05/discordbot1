ab875746c8bad8ef44d2922b794982e0568a47ca
bfan <bfanmbt05@gmail.com>
2021-07-03 11:37:13 -0700

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -11,9 +11,9 @@ module.exports = {
             .setDescription("Below are commands and how to use them!")
             .setThumbnail('https://cdn.discordapp.com/emojis/857732279367761940.png?v=1')
             .addFields(
-                {name: 'User Commands', value: '``join, give, balance, leaderboard, daily, coinflip``'},
-                {name: 'Action Commands', value: '``hug, kiss, spank, slap, punch, throw, lick``'},
-                {name: 'Admin Commands', value: '``adtake, adgive, reset``'},
+                {name: '🔧 User Commands', value: '``join, give, balance, leaderboard, daily, coinflip``'},
+                {name: '🤜 Action Commands', value: '``hug, kiss, spank, slap, punch, throw, lick``'},
+                {name: '👮 Admin Commands', value: '``adtake, adgive, reset``'},
             )
             .setFooter('Created by bfan#0125');
 
@@ -22,21 +22,21 @@ module.exports = {
 
         let map = new Map();
         map.set('join', ['join', 'Join the bot! Using this command will get you on your way to becoming rich!'])
-        map.set('give', ['give/send @[member] [amount]', 'Give another member some of your TMC Cash! Let\'s hope they deserved it...']);
-        map.set('balance', ['balance/bal', 'Check your TMC Cash balance!']);
-        map.set('leaderboard', ['leaderboard/lb [page number]', 'Check the TMC Cash leaderboard! Are you #1?']);
-        map.set('daily', ['daily', 'Claim your daily TMC Cash! Resets every day at 12:00 AM PST.']);
-        map.set('coinflip', ['coinflip/cf [amount] [h/t]', 'Coinflip some TMC Cash and get rich!']);
-        map.set('adtake', ['adtake @[member] [amount]', 'Take some TMC Cash from a member. No regrets!']);
-        map.set('adgive', ['adgive @[member] [amount]', 'Reward someone with TMC Cash!']);
-        map.set('reset', ['reset @[member]', 'Someone has been naughty! Reset their cash.']);
-        map.set('hug', ['hug @[member]', 'Hug someone!']);
-        map.set('kiss', ['kiss @[member]', 'Give someone a big kiss!']);
-        map.set('spank', ['spank @[member]', 'Give someone a fat spank!']);
-        map.set('slap', ['slap @[member]', 'Slap someone!']);
-        map.set('punch', ['punch @[member]', 'Punch another member!']);
-        map.set('throw', ['throw @[member]', 'Throw someone!']);
-        map.set('lick', ['lick @[member]', 'Give someone a wet lick!']);
+        map.set('give', ['give/send @[member] [amount]', 'Give another member some of your TMC Cash! Let\'s hope they deserved it...', '<:KL1Money:860950566268043305>']);
+        map.set('balance', ['balance/bal', 'Check your TMC Cash balance!', '💰']);
+        map.set('leaderboard', ['leaderboard/lb [page number]', 'Check the TMC Cash leaderboard! Are you #1?', '🔝']);
+        map.set('daily', ['daily', 'Claim your daily TMC Cash! Resets every day at 12:00 AM PST.', '⏰']);
+        map.set('coinflip', ['coinflip/cf [amount] [h/t]', 'Coinflip some TMC Cash and get rich!', '🪙']);
+        map.set('adtake', ['adtake @[member] [amount]', 'Take some TMC Cash from a member. No regrets!', '']);
+        map.set('adgive', ['adgive @[member] [amount]', 'Reward someone with TMC Cash!', '']);
+        map.set('reset', ['reset @[member]', 'Someone has been naughty! Reset their cash.', '']);
+        map.set('hug', ['hug @[member]', 'Hug someone!', '🤗']);
+        map.set('kiss', ['kiss @[member]', 'Give someone a big kiss!', '💋']);
+        map.set('spank', ['spank @[member]', 'Give someone a fat spank!', '']);
+        map.set('slap', ['slap @[member]', 'Slap someone!', '<a:TMC_MaiSlapOwO:834498233262800896>']);
+        map.set('punch', ['punch @[member]', 'Punch another member!', '🤜']);
+        map.set('throw', ['throw @[member]', 'Throw someone!', '<a:throw:860952026590871582>']);
+        map.set('lick', ['lick @[member]', 'Give someone a wet lick!', '👅']);
 
         if (!map.has(args[0])) return message.channel.send('That command does not exist!');
         else {
