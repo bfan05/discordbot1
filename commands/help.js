2c9e4ef8b777c2f3150f6aca7672e0c88d2087f1
bfan <bfanmbt05@gmail.com>
2021-07-03 11:07:54 -0700

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -10,20 +10,20 @@ module.exports = {
         .setDescription("Below are commands and how to use them!")
         .setThumbnail('https://cdn.discordapp.com/emojis/857732279367761940.png?v=1')
         .addFields(
-            {name: 'User Commands', value: 'Any user may use these commands!'},
-            {name: '-join', value: 'Join the bot! Using this command will get you on your way to becoming rich!'},
+            {name: 'User Commands', value: '``join, give, balance, leaderboard, daily, coinflip``'},
+            /*{name: '-join', value: 'Join the bot! Using this command will get you on your way to becoming rich!'},
             {name: '-give/send @[member] [amount]', value: 'Give another member some of your TMC Cash! Let\'s hope they deserved it...'},
             {name: '-balance/bal', value: 'Check your TMC Cash balance!'},
             {name: '-leaderboard/lb [page number]', value: 'Check the TMC Cash leaderboard! Are you #1?'},
             {name: '-daily', value: 'Claim your daily TMC Cash! Resets every day at 12:00 AM PST.'},
             {name: '-coinflip/cf [amount] [h/t]', value: 'Coinflip some TMC Cash and get rich!'},
-            {name: '\u200B', value: '\u200B'},
-            {name: 'Action Commands', value: 'hug, kiss, spank, slap, kick, throw, lick'},
-            {name: '\u200B', value: '\u200B'},
-            {name: 'Admin Commands', value: 'Only for admins!'},
-            {name: '-adtake @[member] [amount]', value: 'Take some TMC Cash from a member. No regrets!'},
+            {name: '\u200B', value: '\u200B'},*/
+            {name: 'Action Commands', value: '``hug, kiss, spank, slap, kick, throw, lick``'},
+            /*{name: '\u200B', value: '\u200B'},*/
+            {name: 'Admin Commands', value: '``adtake, adgive, reset``'},
+            /*{name: '-adtake @[member] [amount]', value: 'Take some TMC Cash from a member. No regrets!'},
             {name: '-adgive @[member] [amount]', value: 'Reward someone with TMC Cash!'},
-            {name: '-reset @[member]', value: 'Someone has been naughty! Reset their cash.'},
+            {name: '-reset @[member]', value: 'Someone has been naughty! Reset their cash.'},*/
         )
         .setFooter('Created by bfan#0125');
 
