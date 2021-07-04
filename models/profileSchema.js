3a01f0dcb5537d1fc0ca8072e43005d295edc2f6
bfan <bfanmbt05@gmail.com>
2021-07-03 23:11:49 -0700

    Update profileSchema.js

--- models/profileSchema.js
+++ models/profileSchema.js
@@ -6,6 +6,7 @@ const profileSchema = new mongoose.Schema({
     usernm: {type: String, require: true, unique: false},
     coins: {type: Number, default: 0},
     total: {type: Number, default: 0},
+    dogs: {type: Number, default: 0},
 })
 
 const model = mongoose.model('ProfileModels', profileSchema);
