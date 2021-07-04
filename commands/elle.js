f3900c8a6d3072d884b3ca21f5c42d57ac848968
bfan <bfanmbt05@gmail.com>
2021-07-03 23:01:11 -0700

    Update elle.js

--- commands/elle.js
+++ commands/elle.js
@@ -12,9 +12,9 @@ module.exports = {
     async execute(client, message, args, Discord, profileData) {
         let gifs = [
             'https://media1.tenor.com/images/ecfbb852d71e4aee31453d6b21281c16/tenor.gif?itemid=22029090',
-            'https://media.tenor.com/images/0978ab200183c86a3597537a341b5852/tenor.gif',
-            'https://media.tenor.com/images/12e5d2f27bee47c0cf66892c5ff6e510/tenor.gif',
-            'https://media.tenor.com/images/c1dcf65526afeb3761a7a62887000318/tenor.gif'
+            'https://media.tenor.com/images/8fc863bbcd2eec44e4b868d10fbe9d61/tenor.gif',
+            'https://media1.tenor.com/images/3e8d78df0c61ac3ec9437e6274359f93/tenor.gif?itemid=19144402',
+            'https://media1.tenor.com/images/2e86d5d444afab63daf3b97177b5cd1a/tenor.gif?itemid=19298968'
         ]
 
         let randomImage = randomNumber(0, gifs.length - 1);
