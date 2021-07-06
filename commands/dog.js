388d4a23fd54e191624894b2086ce09999635da0
bfan <bfanmbt05@gmail.com>
2021-07-05 21:48:32 -0700

    Update dog.js

--- commands/dog.js
+++ commands/dog.js
@@ -3,6 +3,7 @@ const profileModel = require("../models/profileSchema");
 module.exports = {
     name: 'dog',
     aliases: [''],
+    cooldown: 10,
     permissions: [],
     description: 'Check the number of dogs you own!',
     async execute(client, message, args, Discord, profileData) {
