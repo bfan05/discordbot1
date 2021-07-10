208265096539967553b2354e11a1d2130b3b5a7f
bfan <bfanmbt05@gmail.com>
2021-07-09 17:30:21 -0700

    Update easteregg.js

--- commands/easteregg.js
+++ commands/easteregg.js
@@ -25,11 +25,11 @@ module.exports = {
         else if (!args[0] && data.easteregg == 0) {
             let correct = false;
 
-            message.author.send("You have discovered the easter egg! If you want it, first answer this question: Magic Number");
+            message.author.send("You have discovered the easter egg! If you want it, first answer this question: What is the \"Magic number\" and the total number of the reward for a perfect Pac-Man game?");
             message.author.createDM().then(dmchannel => {
                 const collector = new Discord.MessageCollector(dmchannel, m => m.author.id === message.author.id, { max: 1, time: 10000 });
                 collector.on('collect', message => {
-                    if (message.content.toLowerCase() == "faith and hope and charity") {
+                    if (message.content.toLowerCase() == "faith and hope and charity. 25") {
                         correct = true;
                         dmchannel.send(`Congratulations! You have received a golden easter egg!`)
                     } else {
