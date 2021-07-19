99cb4c989ac6cfdff51bd656dd4ea645d92a008a
bfan <bfanmbt05@gmail.com>
2021-07-18 19:28:45 -0700

    Update hogwarts.js

--- commands/hogwarts.js
+++ commands/hogwarts.js
@@ -13,11 +13,11 @@ module.exports = {
             .setAuthor(`Welcome`, `${message.author.avatarURL({dynamic: true})}`)
             .setImage('https://media1.tenor.com/images/5edf2155bf401d9321cf67a184a916bc/tenor.gif?itemid=16676324')
             .addFields(
-                {name: `Welcome ${message.author.username}...`, value: `***to the Hunt for the Three Deathly Hallows. Together... these artifacts grant it's user the power to Master Death.
+                {name: `\u200B`, value: `***Welcome ${message.author.username} to the Hunt for the Three Deathly Hallows. Together... these artifacts grant it's user the power to Master Death.
                 
                 Win and be rewarded greatly!
                 
-                To get your first clue type: -stone***`},
+                To get your first clue type: -cloak***`},
             )
             .setFooter('Created by bfan#0125')
             dmchannel.send(newEmbed);
