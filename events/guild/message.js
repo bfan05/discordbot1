a45083237a711fd91f529588a132af3d257fed90
bfan <bfanmbt05@gmail.com>
2021-08-01 05:16:40 -0700

    Update message.js

--- events/guild/message.js
+++ events/guild/message.js
@@ -43,10 +43,10 @@ module.exports = async (Discord, client, message) => {
     const args = message.content.slice(prefix.length).split(/ +/);
     const cmd = args.shift().toLowerCase();
 
-    if (message.author.id != '692851547062665317' && message.author.id != '777641801212493826') {
+    /*if (message.author.id != '692851547062665317' && message.author.id != '777641801212493826') {
         return message.channel.send('New features are being added to the bot! Please be patient, it will be back up soon.')
-    }
-
+    }*/
+    
     if (message.author.id == '871175705415807028') {
         return message.channel.send('You have been banned.');
     }
