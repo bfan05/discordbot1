be33294aee3ce184c95a3d46171c6e8467d1aa26
bfan <bfanmbt05@gmail.com>
2021-08-13 11:10:38 -0700

    Update message.js

--- events/guild/message.js
+++ events/guild/message.js
@@ -16,9 +16,9 @@ module.exports = async (Discord, client, message) => {
         })
     }
 
-    if (message.author.id === '455171049173286923' && message === 'k') {
+    if (message.author.id === '692851547062665317' && message === 'k') {
         message.delete(1000);
-        message.channel.send('k redlotus');
+        message.channel.send('k bfan');
     }
 
     if (!message.content.startsWith(prefix) || message.author.bot) return;
